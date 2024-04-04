414f0d7dae16a8e1e6af1aca1c67a3a61394da4a
Carson <gamercarsonzero@gmail.com>
2024-04-03 23:19:56 -0400

    Add a fuzziness parameter

--- src/main.zig
+++ src/main.zig
@@ -18,16 +18,19 @@ pub fn main() !void {
 
     var material_ground = Lambertian{ .albedo = zm.F32x4{ 0.8, 0.8, 0.0, 0.0 }, .rnd = rnd.random() };
     var material_center = Lambertian{ .albedo = zm.F32x4{ 0.7, 0.3, 0.3, 0.0 }, .rnd = rnd.random() };
-    var material_left = Metal{ .albedo = zm.F32x4{ 0.8, 0.8, 0.8, 0.0 } };
-    var material_right = Metal{ .albedo = zm.F32x4{ 0.8, 0.6, 0.2, 0.0 } };
+    var material_left = Metal{ .albedo = zm.F32x4{ 0.8, 0.8, 0.8, 0.0 }, .fuzz = 0.3, .rnd = rnd.random() };
+    var material_right = Metal{ .albedo = zm.F32x4{ 0.8, 0.6, 0.2, 0.0 }, .fuzz = 1.0, .rnd = rnd.random() };
 
     // TODO: These copy so maybe pass in pointers or construct in-place (emplace) instead?
     var ground = Sphere{ .center = zm.F32x4{ 0.0, -100.5, -1.0, 0.0 }, .radius = zm.f32x4s(100.0), .mat = material_ground.interface() };
     try world.add(ground.interface());
+
     var center_sphere = Sphere{ .center = zm.F32x4{ 0.0, 0.0, -1.0, 0.0 }, .radius = zm.f32x4s(0.5), .mat = material_center.interface() };
     try world.add(center_sphere.interface());
+
     var left_sphere = Sphere{ .center = zm.F32x4{ -1.0, 0.0, -1.0, 0.0 }, .radius = zm.f32x4s(0.5), .mat = material_left.interface() };
     try world.add(left_sphere.interface());
+
     var right_sphere = Sphere{ .center = zm.F32x4{ 1.0, 0.0, -1.0, 0.0 }, .radius = zm.f32x4s(0.5), .mat = material_right.interface() };
     try world.add(right_sphere.interface());
 
--- src/material/metal.zig
+++ src/material/metal.zig
@@ -1,5 +1,6 @@
 //! Metal Material class
 //! This material reflects like a mirror
+//! Also allows for a tweakable amount of fuzz
 
 const std = @import("std");
 const zm = @import("zmath");
@@ -10,6 +11,10 @@ const vector_util = @import("../vector_util.zig");
 
 /// Color that we attenuate the light by.
 albedo: zm.F32x4,
+/// Fuzz Parameter
+fuzz: f32,
+/// Pass in a reference to a random number generator
+rnd: std.rand.Random,
 
 const Metal = @This();
 
@@ -24,7 +29,7 @@ pub fn scatter(self_opaque: *anyopaque, r_in: Ray, rec: *hittable.HitRecord, att
     var self = @as(*Metal, @ptrCast(@alignCast(self_opaque)));
 
     const reflected = vector_util.reflect(zm.normalize4(r_in.dir), rec.normal);
-    scattered.* = Ray{ .orig = rec.p, .dir = reflected };
+    scattered.* = Ray{ .orig = rec.p, .dir = reflected + zm.f32x4s(self.fuzz) * vector_util.random_unit_vector(self.rnd) };
     attenuation.* = self.albedo;
-    return true;
+    return zm.all(zm.dot4(scattered.dir, rec.normal) > zm.f32x4s(0), 4);
 }
